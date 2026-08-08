7e26aa2d0a9b6a1e547ca4a0488aa8b838ff9a71
MANUELA BELLINA CARLESSO <manuela.carlesso@acad.univates>
2026-08-07 21:40:59 -0300

    adiciona coisas na tela

--- aula2/App.js
+++ aula2/App.js
@@ -1,16 +1,29 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button, Pressable, FlatList, TextInput } from 'react-native';
+
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>batata</Text>
-      <StatusBar style="auto" />
+      <Button
+      title='saiba mais'
+      color="FD3DB5"
+      accessibilityLabel='saiba mais sobre este botão'
+      ></Button>
+      <Pressable><Text>ai doi</Text></Pressable>
+    <StatusBar style="auto" />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
+  input: {
+    height: 40,
+    margin: 12,
+    borderWidth: 1,
+    padding: 10,
+  },
   container: {
     flex: 1,
     backgroundColor: '#fff',
